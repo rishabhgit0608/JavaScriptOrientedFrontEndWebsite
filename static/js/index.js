507bad74c75b153526404eb0543fa9541d0b4809
Rishabh Bhambani <bhambanirishabh@protonmail.com>
2020-12-03 02:20:44 +0530

    Adding more JavaScript

--- static/js/index.js
+++ static/js/index.js
@@ -26,8 +26,71 @@ function catGenerator(){
 //3. Challenge 3 Rock paper and Scissors
 
 function rpsgame(yourChoice){
-    var id = yourChoice.id;
-    var text = document.getElementById(id);
-    
+    var humanChoice,Botchoice;
+    humanChoice=yourChoice.id;
+    Botchoice = numberToChoice(choice());
+    alert(Botchoice); 
+    var result = results(Botchoice,humanChoice);
+    console.log(result);
+    var message =finalMessage(result);
+    console.log(message);
+    rpsFrontEnd(yourChoice.id,Botchoice,message);
+}
+function choice(){
+    return Math.floor(Math.random()*3);
+}
+function numberToChoice(number){
+    return ['rock' ,'paper','scissor'][number];
 }
 
+function results(botchoice,humanChoice){
+   var rpsDb = {
+       'rock':{'rock':0.5,'scissor':1, 'paper':0},
+       'paper':{'scissor':0,'rock':1,'paper':0.5},
+       'scissor':{'scissor':0.5, 'rock':0, 'paper':0.5} 
+   }
+   var yourScore = rpsDb[humanChoice][botchoice];
+   var botScore  = rpsDb[botchoice][humanChoice];
+   return [yourScore,botScore];
+}
+
+function finalMessage(result){
+    if(result[0]>result[1]){
+        return {
+            'messagee':'You won',
+            'color':'green'
+        }
+    }
+    else if(result[0]<result[1]){
+        return {
+            'messagee':'You lose',
+            'color':'red'
+        }
+    }
+    else{
+        return {
+            'messagee':'Draw',
+            'color':'blue'
+       }
+    }
+}
+
+function rpsFrontEnd(yourChoice,Botchoice,message){
+    var imgDB = {
+        'rock': document.getElementById('rock').src,
+        'paper':document.getElementById('paper').src,
+        'scissor':document.getElementById('scissor').src
+    }
+    //removing the frontEnd and adding that to the dom
+    document.getElementById('rock').remove();
+    document.getElementById('scissor').remove();
+    document.getElementById('paper').remove();
+    var image = document.createElement('img');
+    var div= document.getElementById('flexBox3');
+    image.src =imgDB[yourChoice.id];
+    div.appendChild(image);
+    var image2 = document.createElement('img');
+    var div2= document.getElementById('flexBox3');
+    image1.src =imgDB[Botchoice.id];
+    div.appendChild(image2);
+}
